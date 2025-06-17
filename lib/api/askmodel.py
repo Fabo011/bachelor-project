715d01438702c0a00d44eac8a77d71e31c9ba9dd
Fabo011 <based011@protonmail.com>
2025-06-17 12:28:19 +0200

    ai workflow working. mcp added.

--- lib/api/askmodel.py
+++ lib/api/askmodel.py
@@ -1,14 +1,36 @@
 from pydantic import BaseModel
 from lib.model.model import load_model
-
-# Load model once
+from fastmcp import Client
+from fastmcp.client.transports import StreamableHttpTransport
+import json
+from dotenv import load_dotenv
+import os
+load_dotenv()
 model = load_model()
 
 class PromptRequest(BaseModel):
     prompt: str
+    
+MCP_URL = os.getenv("MCP_ZAPIER_URL")
+transport = StreamableHttpTransport(MCP_URL)
+client = Client(transport=transport)
 
 def generate_response(prompt: str) -> str:
     response = model.generate(prompt, max_tokens=1000)
     print(f"Prompt: {prompt}")
     print(f"Response: {response}")
     return response
+
+async def send_email_via_mcp(body: str):
+    email = os.getenv("EMAIL")
+    async with client:
+        result = await client.call_tool(
+            "gmail_send_email",
+            {
+                "to": email,
+                "subject": "Plant Watering Reminder",
+                "body": body,
+                "instructions": "Send an email with the provided body.",
+            },
+        )
+        return json.loads(result[0].text)
--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-from lib.api.askmodel import PromptRequest, generate_response
+from lib.api.askmodel import PromptRequest, generate_response, send_email_via_mcp
 
 app = FastAPI()
 
 @app.post("/askmodel")
 async def askmodel(request: PromptRequest):
     llm_response = generate_response(request.prompt)
-    return {"llm_response": llm_response}
+    if "plant needs water" in llm_response.lower():
+      mcp_result = await send_email_via_mcp(llm_response)
+    return {"llm_response": llm_response, "mcp_result": mcp_result}
 
 
 if __name__ == "__main__":
